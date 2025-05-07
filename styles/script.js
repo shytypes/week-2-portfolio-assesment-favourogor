d906f9f289f1f4da431198f86202c6e8208044eb
Favour Ogor <111064648+favourogor@users.noreply.github.com>
2025-05-07 17:30:14 +0100

    Second Commit

---
+++ styles/script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+
+const firstNameInput = document.getElementById("firstName");
+const secondNameInput = document.getElementById("secondName");
+const emailInput = document.getElementById("email");
+const displayArea = document.getElementById("displayArea");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const firstName = 
+    firstNameInput.value.trim();
+    const secondName =
+    secondNameInput.value.trim();
+    const email =
+    emailInput.value.trim();
+
+    const infoDiv =
+    document.createElement("div");
+    infoDiv.innerHTML = 
+      <h3>Submitted info:</h3>
+      <p>First Name: ${firstName}</p>
+      <p>Second Name: ${secondName}</p>
+      <p>Email: ${email}</p>
+      <hr />
+      
+      displayArea.appendChild(infoDiv);
+
+      form.requestFullscreen();
+});
